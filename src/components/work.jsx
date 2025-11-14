50161df85bbb548aea71d1117f5ad37d77990d72
diya-lamba11 <lambadiya9@gmail.com>
2025-11-14 11:18:11 +0530

    Update work.jsx

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,51 +1,59 @@
 import React from "react";
-import { Box, Typography,Divider } from "@mui/material";
+import { Box, Typography, Divider, Container } from "@mui/material";
+
 const Work = () => {
     return (
         <Box
             sx={{
                 backgroundColor: "#172b5b",
                 color: "white",
-                py: { xs: 6, md: 10 },
-                px: { xs: 3, sm: 8, md:3}
+                py: { xs: 1,sm:1.5, md: 1,lg:2 },
+                px: { xs: 1, sm: 1, md: 1,lg:2 }
             }}
         >
-            <Typography
-                variant="h3"
-                sx={{
-                    fontWeight: "bold",
-                    mb: 2,
-                    textTransform: "uppercase",
-                }}
-            >
-                Our Work
-            </Typography>
-            <Divider
-                sx={{
-                    width: 120,
-                    height: 3,
-                    background:
-                        "linear-gradient(90deg, #ff5f2e 0%, rgba(255,95,46,0) 100%)",
-                    mb: 2,
+            <Container maxWidth="xl" sx={{ p: 0 }}>
+                <Typography
+                    variant="h3"
+                    sx={{
+                        fontWeight: "bold",
+                        textTransform: "uppercase",
+                        m: 0,
+                        mb: 1
+                    }}
+                >
+                    Our Work
+                </Typography>
 
-                }}
-            />
-            <Typography
-                variant="h6"
-                sx={{
-                    lineHeight: 1.7,
-                    fontWeight: 500,
-                    fontSize:22,
-                }}
+                <Divider
+                    sx={{
+                        width: 120,
+                        height: 3,
+                        background:
+                            "linear-gradient(90deg, #ff5f2e 0%, rgba(255,95,46,0) 100%)",
+                        m: 0,
+                        mb: 1
+                    }}
+                />
 
-            >
-                Explore our diverse portfolio of Web3, mobile app, and web development
-                projects that showcase our commitment to innovation, quality, and
-                client satisfaction. From cutting-edge blockchain solutions to
-                captivating mobile experiences and stunning web designs, we take pride
-                in delivering excellence with every project.
-            </Typography>
+                <Typography
+                    variant="h6"
+                    sx={{
+                        lineHeight: 1.4,
+                        fontWeight: 500,
+                        fontSize: 22,
+                        m: 0,
+                        mt: 1
+                    }}
+                >
+                    Explore our diverse portfolio of Web3, mobile app, and web development
+                    projects that showcase our commitment to innovation, quality, and
+                    client satisfaction. From cutting-edge blockchain solutions to
+                    captivating mobile experiences and stunning web designs, we take pride
+                    in delivering excellence with every project.
+                </Typography>
+            </Container>
         </Box>
-    )
-}
+    );
+};
+
 export default Work;
